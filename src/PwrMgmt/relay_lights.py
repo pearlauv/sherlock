b9b4f017998081a666be76460e3d531ce2d658c3
Abhishek Bhattiprolu <abhi01@mit.edu>
2024-09-19 17:02:48 -0400

    Updated PwrMgmt

---
+++ src/PwrMgmt/relay_lights.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+import schedule
+import time
+from datetime import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(filename='/home/pi/Sherlock/data/PwrMgmt/relay_control.log', level=logging.INFO, 
+                    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin connected to the relay
+RELAY_PIN = 24  # Using GPIO 24 for this relay
+
+# Setup the GPIO pin as an output
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+def turn_on():
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    logging.info("Night Relay turned ON")
+
+def turn_off():
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    logging.info("Night Relay turned OFF")
+
+def cleanup():
+    GPIO.cleanup()
+    logging.info("GPIO cleanup complete")
+
+# Schedule the relay operations
+schedule.every().day.at("18:00").do(turn_on)  # 6 PM
+schedule.every().day.at("07:00").do(turn_off)  # 7 AM
+
+try:
+    logging.info("Night Relay control script is running.")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}")
+
+finally:
+    cleanup()
---
+++ src/PwrMgmt/relay_starlink.py
@@ -0,0 +1,48 @@
+import RPi.GPIO as GPIO
+import schedule
+import time
+from datetime import datetime
+import logging
+
+# Set up logging
+logging.basicConfig(filename='/home/pi/Sherlock/data/PwrMgmt/relay_control.log', level=logging.INFO, 
+                    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+# Set the GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+# Define the GPIO pin connected to the relay
+RELAY_PIN = 23
+
+# Setup the GPIO pin as an output
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+
+def turn_on():
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    logging.info("Relay turned ON")
+
+def turn_off():
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+    logging.info("Relay turned OFF")
+
+def cleanup():
+    GPIO.cleanup()
+    logging.info("GPIO cleanup complete")
+
+# Schedule the relay operations
+schedule.every().day.at("10:00").do(turn_on)
+schedule.every().day.at("10:30").do(turn_off)
+schedule.every().day.at("16:00").do(turn_on)
+schedule.every().day.at("16:30").do(turn_off)
+
+try:
+    logging.info("Relay control script is running.")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+except Exception as e:
+    logging.error(f"An error occurred: {str(e)}")
+
+finally:
+    cleanup()
