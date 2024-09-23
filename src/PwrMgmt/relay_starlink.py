5a97b6f09c7d37180ae03f7913f2c58cdcbb3650
Abhishek Bhattiprolu <abhi01@mit.edu>
2024-09-23 14:07:40 -0400

    fixed

--- src/PwrMgmt/relay_starlink.py
+++ src/PwrMgmt/relay_starlink.py
@@ -1,16 +1,15 @@
 import RPi.GPIO as GPIO
 import schedule
 import time
-from datetime import datetime
 import logging
 
 # Set up logging
-<<<<<<< HEAD
-logging.basicConfig(filename='/home/pi/Sherlock/data/PwrMgmt/relay_starlink.log', level=logging.INFO, 
-=======
-logging.basicConfig(filename='/home/pi/Sherlock/data/PwrMgmt/relay_control.log', level=logging.INFO, 
->>>>>>> b9b4f017998081a666be76460e3d531ce2d658c3
-                    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+logging.basicConfig(
+    filename='/home/pi/Sherlock/data/PwrMgmt/relay_starlink.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 # Set the GPIO mode to BCM
 GPIO.setmode(GPIO.BCM)
@@ -23,33 +22,24 @@ GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 def turn_on():
     GPIO.output(RELAY_PIN, GPIO.HIGH)
-    logging.info("Relay turned ON")
+    logging.info("Starlink Relay turned ON")
 
 def turn_off():
     GPIO.output(RELAY_PIN, GPIO.LOW)
-    logging.info("Relay turned OFF")
+    logging.info("Starlink Relay turned OFF")
 
 def cleanup():
     GPIO.cleanup()
     logging.info("GPIO cleanup complete")
 
 # Schedule the relay operations
-<<<<<<< HEAD
-schedule.every().day.at("14:15").do(turn_on)
-schedule.every().day.at("14:45").do(turn_off)
-=======
 schedule.every().day.at("10:00").do(turn_on)
 schedule.every().day.at("10:30").do(turn_off)
->>>>>>> b9b4f017998081a666be76460e3d531ce2d658c3
 schedule.every().day.at("16:00").do(turn_on)
 schedule.every().day.at("16:30").do(turn_off)
 
 try:
-<<<<<<< HEAD
     logging.info("Starlink Relay control script is running.")
-=======
-    logging.info("Relay control script is running.")
->>>>>>> b9b4f017998081a666be76460e3d531ce2d658c3
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -58,8 +48,4 @@ except Exception as e:
     logging.error(f"An error occurred: {str(e)}")
 
 finally:
-<<<<<<< HEAD
     cleanup()
-=======
-    cleanup()
->>>>>>> b9b4f017998081a666be76460e3d531ce2d658c3
