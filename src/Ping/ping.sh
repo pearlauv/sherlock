e6ded88746c491c7e842e5abde7bf71ea4a48175
Abhishek Bhattiprolu <abhi01@mit.edu>
2024-09-29 23:56:44 -0400

    Enhanced Ping Monitoring System

--- src/Ping/ping.sh
+++ src/Ping/ping.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 LOGFILE="/home/pi/Sherlock/data/Ping/ping_log.csv"
 HOST1="8.8.8.8"
-HOST2="10.49.0.2"
-HOST3="10.49.0.4"
-HOST4="10.49.0.9"
+HOST2="10.49.0.1"
+HOST3="10.49.0.2"
+HOST4="10.49.0.4"
+HOST5="10.49.0.9"
 while true; do
     echo "$(date): Google       $(ping -c 1 $HOST1 | grep 'time=')" >> $LOGFILE
-    echo "$(date): Oyster       $(ping -c 1 $HOST2 | grep 'time=')" >> $LOGFILE
-    echo "$(date): NavPi        $(ping -c 1 $HOST3 | grep 'time=')" >> $LOGFILE
-    echo "$(date): PantherPi    $(ping -c 1 $HOST4 | grep 'time=')" >> $LOGFILE
+    echo "$(date): ServerPi     $(ping -c 1 $HOST2 | grep 'time=')" >> $LOGFILE
+    echo "$(date): Oyster       $(ping -c 1 $HOST3 | grep 'time=')" >> $LOGFILE
+    echo "$(date): NavPi        $(ping -c 1 $HOST4 | grep 'time=')" >> $LOGFILE
+    echo "$(date): PantherPi    $(ping -c 1 $HOST5 | grep 'time=')" >> $LOGFILE
     sleep 30  # Ping every 30 seconds
 done
